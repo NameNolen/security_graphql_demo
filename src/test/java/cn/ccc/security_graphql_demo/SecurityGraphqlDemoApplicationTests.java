8bd5f7da88792d1fab943fb003ceff4e155e5757
NameNolen <renpeng18@live.com>
2025-06-14 17:11:20 +0800

    init

---
+++ src/test/java/cn/ccc/security_graphql_demo/SecurityGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ccc.security_graphql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityGraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
